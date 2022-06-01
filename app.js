0ad29e9f82323b1dcae1c644ec5805675e0faa75
Kamil <slaboszkamil@gmail.com>
2022-06-01 11:35:56 +0200

    Add Users

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require('passport');
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -13,18 +17,93 @@ app.use(express.static("public"));
 
 mongoose.connect('mongodb://localhost:27017/trainingDB');
 
-const userSchema = {
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const userSchema = new mongoose.Schema ({
   name: String,
   password: String,
   videos: [],
-}
+});
+
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+  User.findById(id, function(err, user){
+    done(err, user);
+  });
+});
 
 app.route("/")
 
 .get(function(req, res){
   res.render("home");
+});
+
+// LOGIN AND REGISTER MANAGEMENT //////////////////////////////////////
+
+app.route("/register")
+.get(function(req, res){
+  res.render("register");
+})
+
+.post(function(req, res){
+
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        // res.redirect("/secrets");
+      })
+    }
+  })
+
+});
+
+app.route("/login")
+.get(function(req, res){
+  res.render("login");
+})
+
+.post(function(req, res){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.login(user, function(err){
+    if (err){
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        // res.redirect("/secrets");
+      })
+    }
+  })
+});
+
+app.get("/logout", function(req, res, next){
+  req.logout(function(err) {
+    if (err) {
+       return next(err)
+     }
+   });
+  res.redirect("/");
 });
 
 app.route("/add-video/:userId")
