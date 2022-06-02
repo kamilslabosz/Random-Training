34a306cf514a9f00e9592ea41c2a707a9713d742
Kamil <slaboszkamil@gmail.com>
2022-06-02 12:10:27 +0200

    prepare app to deploy

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/trainingDB');
+mongoose.connect(process.env.MONGODB_ADDRESS);
 
 app.use(session({
   secret: process.env.SECRET,
@@ -184,6 +184,11 @@ app.post('/change-password/:userId', function(req, res){
   res.redirect("/dashboard")
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
